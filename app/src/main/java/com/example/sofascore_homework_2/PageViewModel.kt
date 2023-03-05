a47fc9ca4e89ce4bda84c05045a7b8e118d22321
dariovidovic <malivida00@gmail.com>
2023-03-05 22:08:05 +0100

    Connect fragments with ViewModel

--- app/src/main/java/com/example/sofascore_homework_2/PageViewModel.kt
+++ app/src/main/java/com/example/sofascore_homework_2/PageViewModel.kt
@@ -2,6 +2,41 @@ package com.example.sofascore_homework_2
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
- class PageViewModel: ViewModel() {
 
- }
+class PageViewModel : ViewModel() {
+
+    private val liveData = MutableLiveData<List<Person>>(emptyList())
+
+    fun getLiveData() : LiveData<List<Person>>{
+        return liveData
+    }
+
+    fun addPerson(
+        firstName: String,
+        lastName: String,
+        age: Int,
+        oib: Int,
+        birthPlace: String,
+        currentOccupation: String,
+        favouriteMovie: String,
+        favouriteTvShow: String,
+        favouriteFood: String,
+        favouriteSong: String
+    ) {
+        val list = liveData.value?.toMutableList()
+        liveData.value = list
+        val person = Person(
+            firstName,
+            lastName,
+            age,
+            oib,
+            birthPlace,
+            currentOccupation,
+            favouriteMovie,
+            favouriteTvShow,
+            favouriteFood,
+            favouriteSong
+        )
+        list?.add(person)
+    }
+}
