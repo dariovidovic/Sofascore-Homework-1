d6e66f6c062bb2be207540bf73bfb3c6f30e174d
dariovidovic <malivida00@gmail.com>
2023-03-06 01:11:29 +0100

    Add EditTexts to First Fragment + ScrollView

--- app/src/main/java/com/example/sofascore_homework_2/FirstFragment.kt
+++ app/src/main/java/com/example/sofascore_homework_2/FirstFragment.kt
@@ -7,17 +7,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import com.example.sofascore_homework_2.databinding.FragmentFirstBinding
+import com.example.sofascore_homework_2.databinding.FragmentSecondBinding
 
 
 class FirstFragment : Fragment() {
-
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
     private val viewModel: PageViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel.addPerson(
+        /*viewModel.addPerson(
             "dario",
             "vidovic",
             22,
@@ -28,9 +31,40 @@ class FirstFragment : Fragment() {
             "chernobyl",
             "pizza",
             "never gonna give you up"
-        )
+        )*/
 
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.run {
+            button.setOnClickListener {
+                viewModel.addPerson(
+                    enterFirstName.text.toString(),
+                    enterSecondName.text.toString(),
+                    enterAge.text.toString().toIntOrNull() ?: 0,
+                    enterOib.text.toString().toIntOrNull() ?: 0,
+                    enterBirthplace.text.toString(),
+                    enterCurrOccupation.text.toString(),
+                    enterFavMovie.text.toString(),
+                    enterFavTvShow.text.toString(),
+                    enterFavFood.text.toString(),
+                    enterFavSong.text.toString()
+                )
+                enterFirstName.text.clear()
+                enterSecondName.text.clear()
+                enterAge.text.clear()
+                enterOib.text.clear()
+                enterBirthplace.text.clear()
+                enterCurrOccupation.text.clear()
+                enterFavMovie.text.clear()
+                enterFavTvShow.text.clear()
+                enterFavFood.text.clear()
+                enterFavSong.text.clear()
+            }
+        }
+
+
+        return view
 
 
     }
@@ -38,5 +72,5 @@ class FirstFragment : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance() = FirstFragment()
-            }
     }
+}
--- app/src/main/java/com/example/sofascore_homework_2/Person.kt
+++ app/src/main/java/com/example/sofascore_homework_2/Person.kt
@@ -1,11 +1,19 @@
 package com.example.sofascore_homework_2
 
-data class Person(val firstName: String, val lastName: String,
-                  val age: Int, val oib: Int, val birthPlace: String, val currentOccupation: String,
-                  val favouriteMovie: String, val favouriteTvShow: String, val favouriteFood: String, val favouriteSong: String
-){
+data class Person(
+    val firstName: String,
+    val lastName: String,
+    val age: Int,
+    val oib: Int,
+    val birthPlace: String,
+    val currentOccupation: String,
+    val favouriteMovie: String,
+    val favouriteTvShow: String,
+    val favouriteFood: String,
+    val favouriteSong: String
+) {
     override fun toString(): String {
-        return  "$firstName, $lastName, $age, $oib, $birthPlace," +
+        return "$firstName, $lastName, $age, $oib, $birthPlace," +
                 "$currentOccupation, $favouriteMovie, $favouriteTvShow, $favouriteFood, $favouriteSong"
     }
 }
